ec104e321e8cfc7245c8a68c130cf8e5b302f776
Magdusz <goferek23@gmail.com>
2017-03-05 00:00:42 +0100

    data factory, realm fix

---
+++ Nom/RealmData.swift
@@ -0,0 +1,62 @@
+//
+//  RealmData.swift
+//  Nom
+//
+//  Created by Magdusz on 04.03.2017.
+//  Copyright © 2017 Magdusz. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+struct RealmData {
+    
+    static func getHindusMealsData() -> (names: [String], prices: [String], ingredients: [String]) {
+        let names = ["Chicken Vindaloo", "Butter Chicken", "Chicken Korma", "Rajma Masala", "Chicken Palak", "Chicken Tikka Masala", "Chana Masala"]
+        let prices = ["12/15", "12/15", "12/15", "12/15", "12/15", "12/15", "12/15"]
+        let ingredients = ["Ostry 5/5: Pierś z kurczaka w ostrym sosie curry z ryżem basmati.", "Ostry 1/5: Pierś z kurczaka w sosie maślano-pomidorowym z ryżem basmati.", "Ostry 2/5: Pierś z kurczaka w sosie jogurtowo-kokosowym z ryżem basmati.",  "Ostry 2/5: Czerwona fasolka w gęstym sosie pomidorowym z ryżem basmati.", "Ostry 3/5: Pierś z kurczaka w indyjskim sosie szpinakowym z ryżem basmati.", "Ostry 4/5: Pierś z kurczaka w sosie tikka masala z ryżem basmati.", "Ostry 2/5: Ciecierzyca w pomidorowym sosie curry z ryżem basmati."]
+        return (names, prices, ingredients)
+    }
+    
+    static func getManufakturaMealsData() -> (names: [String], prices: [String], ingredients: [String]) {
+        let names = ["Burger classic", "Cheeseburger", "Burger wiejski", "Krak burger", "Black Jack", "Frytki belgijskie"]
+        let prices = ["17", "18", "21", "22", "31", "7/9"]
+        let ingredients = ["Bułka jasna, majonez, sałata, mięso wołowe, pomidor, cebula, ketchup", "Bułka jasna, majonez, ogórek, sałata, mięso wołowe, ser cheddar, pomidor, cebula, ketchup", "Bułka pełnoziarnista, majonez, ogórek, sałata, mięso wołowe, ser wędzony, bekon, placek ziemniaczany, pomidor, cebula, ketchup, jajko",  "HOT. Bułka jasna, majonez, ogórek, sałata, mięso wołowe, ser cheddar, bekon, papryczki jalapeño, pomidor, cebula, sos ognisty", "Burger ze wszytkimi dodatkami", "180/300g + sos (miodowo-musztardowy, barbecue, ognisty, żurawinowy, majonezowo-koperkowy, majonez"]
+        return (names, prices, ingredients)
+    }
+
+    static func getPizzaMealsData() -> (names: [String], prices: [String], ingredients: [String]) {
+        let names = ["Margherita", "Capri", "Salami", "Salami Plus", "Hawaja", "Wege", "Ostry Rocco", "Maxi", "Mięsna"]
+        let prices = ["13/22", "14/25", "14/25", "14/25", "15/27", "15/27", "16/28", "16/30", "17/32"]
+        let ingredients = ["27/42 cm. Sos, mozarella, rukola", "27/42 cm. Sos, mozarella, szynka, pieczarki", "27/42 cm. Sos, mozarella, 2x salami", "27/42 cm. Sos, mozarella, salami, pieczarki", "27/42 cm. Sos, mozarella, szynka ananas", "27/42 cm. Sos, mozarella, kukurydza, pomidor, rukola", "27/42 cm. Sos, mozarella, salami, jalapeño, cebula", "27/42 cm. Sos, mozarella, salami, szynka, pieczarki", "27/42 cm. Sos, mozarella, salami, szynka, pieczarki, boczek, cebula"]
+        return (names, prices, ingredients)
+    }
+
+    static func getMajorMealsData() -> (names: [String], prices: [String], ingredients: [String]) {
+        let names = ["Klasyk", "Ser", "Chłop", "Baba", "Francuz", "Major", "Kurczak w pomidorach", "Kurczak w pieczarkach", "Grek", "Pierogi"]
+        let prices = ["13,50", "14,50", "16", "16", "16", "18", "16", "16", "18", "10"]
+        let ingredients = ["Wołowina, sałata, czerwona cebula, pomidor, pikle, ketchup, majonez, bułka", "Wołowina, ser cheddar, sałata, czerwona cebula, pomidor, pikle, ketchup, majonez, bułka", "Wołowina, ser cheddar, bekon, sałata, czerwona cebula, pomidor, pikle, ketchup, sos BBQ, bułka", "Wołowina, ser wędzony, żurawina, karmelizowana cebula, sałata, rukola, ketchup, bułka", "Wołowina, ser pleśniowy, karmelizowana cebula, pomidor, sałata, rukola, ketchup, bułka", "Wołowina, ser cheddar, bekon, papryka marynowana, papryka peperoni, sałata, karmelizowana cebula, pomidor, ketchup, sos cayenne (ostry), bułka", "Filet z piersi kurczaka, ser mozarella, suszone pomidory, czerwona cebula, pesto, sałata, rukola, ketchup, majonez, bułka", "Filet z piersi kurczaka w curry, ser cheddar, pieczarki duszone, bekon, czerwona cebula, sałata, ketchup, majonez, bułka", "Wołowina, tzatziki, ser feta, cebula czerwona, pomidor, sałata, ketchup, bułka", "10 sztuk: ruskie, z mięsem, ze szpinakiem, z pieczarkami, z kapustą i grzybami, Gajowego - kasza, grzyby, boczek"]
+        return (names, prices, ingredients)
+    }
+    
+    static func getOwcaMealsData() -> (names: [String], prices: [String], ingredients: [String]) {
+        let names = ["Owca w biegu", "Owca na wypasie", "Owca vege"]
+        let prices = ["10/18", "22", "10/15"]
+        let ingredients = ["Kefta z mięsa jagnięcego, sos pomidorowo-kolendrowy z ciecierzycą, sałata z dwoma rodzajami sosów jogurtowych (pikantnym i sezamowo cytrynowym), grillowana pita", "Kefta z mięsa jagnięcego, falafele, sos pomidorowo-kolendrowy z ciecierzycą, sałata z dwoma rodzajami sosów jogurtowych (pikantnym i sezamowo cytrynowym), grillowana pita", "Falafele, sos pomidorowo-kolendrowy z ciecierzycą, sałata z dwoma rodzajami sosów jogurtowych (pikantnym i sezamowo cytrynowym), grillowana pita"]
+        return (names, prices, ingredients)
+    }
+    
+    static func getFoodTrucksData() -> (names: [String], phones: [String], meals: [[MealModel]]) {
+        let names = ["Hindus", "Manufaktura Krakowska", "Pizza z pieca", "Major Catering", "Rozbrykana Owca"]
+        let phones = ["", "518 527 253", "", "692 830 954", ""]
+        let meals = [RealmManager.getMeals(mealNames: getHindusMealsData().names), RealmManager.getMeals(mealNames: getManufakturaMealsData().names), RealmManager.getMeals(mealNames: getPizzaMealsData().names), RealmManager.getMeals(mealNames: getMajorMealsData().names), RealmManager.getMeals(mealNames: getOwcaMealsData().names)]
+        return (names, phones, meals)
+    }
+    
+    static func getDaysData() -> (names: [String], colors: [String], foodTrucks: [[FoodTruckModel]]) {
+        let names = ["Poniedziałek", "Czwartek"]
+        let colors = [DayColors.blue.rawValue, DayColors.orange.rawValue]
+        let foodTrucks = [RealmManager.getFoodTrucks(foodTruckNames: ["Hindus", "Pizza z pieca", "Major Catering"]), RealmManager.getFoodTrucks(foodTruckNames: ["Manufaktura Krakowska", "Pizza z pieca", "Major Catering", "Rozbrykana Owca"])]
+         return (names, colors, foodTrucks)
+    }
+}
--- Nom/RealmManager.swift
+++ Nom/RealmManager.swift
@@ -20,110 +20,76 @@ struct RealmManager {
     }
     
     private static func createDatabase() {
-        try! realm().write() {
-            var monday = realm().add(mondayData())
-            let thursday = realm().add(thursdayData())
+        //meals
+        createMeals(names: RealmData.getHindusMealsData().names, prices: RealmData.getHindusMealsData().prices, ingredients: RealmData.getHindusMealsData().ingredients)
+        createMeals(names: RealmData.getManufakturaMealsData().names, prices: RealmData.getManufakturaMealsData().prices, ingredients: RealmData.getManufakturaMealsData().ingredients)
+        createMeals(names: RealmData.getPizzaMealsData().names, prices: RealmData.getPizzaMealsData().prices, ingredients: RealmData.getPizzaMealsData().ingredients)
+        createMeals(names: RealmData.getMajorMealsData().names, prices: RealmData.getMajorMealsData().prices, ingredients: RealmData.getMajorMealsData().ingredients)
+        createMeals(names: RealmData.getOwcaMealsData().names, prices: RealmData.getOwcaMealsData().prices, ingredients: RealmData.getOwcaMealsData().ingredients)
+        //food trucks
+        createFoodTrucks(names: RealmData.getFoodTrucksData().names, phones: RealmData.getFoodTrucksData().phones, meals: RealmData.getFoodTrucksData().meals)
+        //days
+        createDays(names: RealmData.getDaysData().names, colors: RealmData.getDaysData().colors, foodTrucks: RealmData.getDaysData().foodTrucks)
+    }
+    
+    private static func createMeals(names: [String], prices: [String], ingredients: [String]) {
+        if !((names.count == prices.count) && (prices.count == ingredients.count)) {
+            print("creating meals array error")
+            return
+        }
+        
+        for i in 0..<names.count {
+            let meal = MealModel(value: [names[i], prices[i], ingredients[i]])
+            try! realm().write {
+                realm().add(meal)
+            }
         }
     }
     
-    private static func hindusData() -> FoodTruckModel {
+    static func getMeals(mealNames: [String]) -> [MealModel] {
+        var meals = [MealModel]()
+        for name in mealNames {
+            let meal = realm().object(ofType: MealModel.self, forPrimaryKey: name) ?? MealModel()
+            meals.append(meal)
+        }
+        return meals
+    }
+    
+    private static func createFoodTrucks(names: [String], phones: [String], meals: [[MealModel]]) {
+        if !((names.count == phones.count) && (phones.count == meals.count)) {
+            print("creating meals array error")
+            return
+        }
         
-        let hindusMeals: [MealModel] = [
-            MealModel(value: ["Chicken Vindaloo", "12/15", "Ostry 5/5: Pierś z kurczaka w ostrym sosie curry z ryżem basmati."]),
-            MealModel(value: ["Butter Chicken", "11/14", "Ostry 1/5: Pierś z kurczaka w sosie maślano-pomidorowym z ryżem basmati."]),
-            MealModel(value: ["Chicken Korma", "12/15", "Ostry 2/5: Pierś z kurczaka w sosie jogurtowo-kokosowym z ryżem basmati."]),
-            MealModel(value: ["Rajma Masala", "11/14", "Ostry 2/5: Czerwona fasolka w gęstym sosie pomidorowym z ryżem basmati."]),
-            MealModel(value: ["Chicken Palak", "12/15", "Ostry 3/5: Pierś z kurczaka w indyjskim sosie szpinakowym z ryżem basmati."]),
-            MealModel(value: ["Chicken Tikka Masala", "12/15", "Ostry 4/5: Pierś z kurczaka w sosie tikka masala z ryżem basmati."]),
-            MealModel(value: ["Chana Masala", "11/14", "Ostry 2/5: Ciecierzyca w pomidorowym sosie curry z ryżem basmati."])
-        ]
+        for i in 0..<names.count {
+            let foodTruck = FoodTruckModel(value: [names[i], phones[i], meals[i]])
+            try! realm().write {
+                realm().add(foodTruck)
+            }
+        }
+    }
+    
+    static func getFoodTrucks(foodTruckNames: [String]) -> [FoodTruckModel] {
+        var foodTrucks = [FoodTruckModel]()
+        for name in foodTruckNames {
+            let foodTruck = realm().object(ofType: FoodTruckModel.self, forPrimaryKey: name) ?? FoodTruckModel()
+            foodTrucks.append(foodTruck)
+        }
+        return foodTrucks
+    }
 
-        let hindus: [Any?] = ["Hindus", "12 346 67 95", hindusMeals]
-        return FoodTruckModel(value: hindus)
-    }
-    
-    private static func manufakturaData() -> FoodTruckModel {
-        let manufakturaMeals: [MealModel] = [
-            MealModel(value: ["Burger classic", "17", "Bułka jasna, majonez, sałata, mięso wołowe, pomidor, cebula, ketchup"]),
-            MealModel(value: ["Cheeseburger", "18", "Bułka jasna, majonez, ogórek, sałata, mięso wołowe, ser cheddar, pomidor, cebula, ketchup"]),
-            MealModel(value: ["Burger wiejski", "21", "Bułka pełnoziarnista, majonez, ogórek, sałata, mięso wołowe, ser wędzony, bekon, placek ziemniaczany, pomidor, cebula, ketchup, jajko"]),
-            MealModel(value: ["Krak burger", "22", "HOT. Bułka jasna, majonez, ogórek, sałata, mięso wołowe, ser cheddar, bekon, papryczki jalapeño, pomidor, cebula, sos ognisty"]),
-            MealModel(value: ["Black Jack", "31", "Burger ze wszytkimi dodatkami"]),
-            MealModel(value: ["Frytki belgijskie", "7/9", "180/300g + sos (miodowo-musztardowy, barbecue, ognisty, żurawinowy, majonezowo-koperkowy, majonez"])
-            ]
+    private static func createDays(names: [String], colors: [String], foodTrucks: [[FoodTruckModel]]) {
+        if !((names.count == colors.count) && (colors.count == foodTrucks.count)) {
+            print("creating meals array error")
+            return
+        }
         
-        let manufaktura: [Any?] = ["Manufaktura Krakowska", "518 527 253", manufakturaMeals]
-        return FoodTruckModel(value: manufaktura)
-    }
-    
-    private static func pizzaZPiecaData() -> FoodTruckModel {
-        let pizzaZpiecaMeals: [MealModel] = [
-            MealModel(value: ["Margherita", "13/22", "27/42 cm. Sos, mozarella, rukola"]),
-            MealModel(value: ["Capri", "14/25", "27/42 cm. Sos, mozarella, szynka, pieczarki"]),
-            MealModel(value: ["Salami", "14/25", "27/42 cm. Sos, mozarella, 2x salami"]),
-            MealModel(value: ["Salami Plus", "14/25", "27/42 cm. Sos, mozarella, salami, pieczarki"]),
-            MealModel(value: ["Hawaja", "15/27", "27/42 cm. Sos, mozarella, szynka ananas"]),
-            MealModel(value: ["Wege", "15/27", "27/42 cm. Sos, mozarella, kukurydza, pomidor, rukola"]),
-            MealModel(value: ["Ostry Rocco", "16/28", "27/42 cm. Sos, mozarella, salami, jalapeño, cebula"]),
-            MealModel(value: ["Maxi", "16/30", "27/42 cm. Sos, mozarella, salami, szynka, pieczarki"]),
-            MealModel(value: ["Mięsna", "17/32", "27/42 cm. Sos, mozarella, salami, szynka, pieczarki, boczek, cebula"])
-        ]
-        
-        let pizzaZPieca: [Any?] = ["Pizza z pieca", "", pizzaZpiecaMeals]
-        return FoodTruckModel(value: pizzaZPieca)
-    }
-    
-    private static func majorBurgerData() -> FoodTruckModel {
-        let majorMeals: [MealModel] = [
-            MealModel(value: ["Klasyk", "13,50", "Wołowina, sałata, czerwona cebula, pomidor, pikle, ketchup, majonez, bułka"]),
-            MealModel(value: ["Ser", "14,50", "Wołowina, ser cheddar, sałata, czerwona cebula, pomidor, pikle, ketchup, majonez, bułka"]),
-            MealModel(value: ["Chłop", "16", "Wołowina, ser cheddar, bekon, sałata, czerwona cebula, pomidor, pikle, ketchup, sos BBQ, bułka"]),
-            MealModel(value: ["Baba", "16", "Wołowina, ser wędzony, żurawina, karmelizowana cebula, sałata, rukola, ketchup, bułka"]),
-            MealModel(value: ["Francuz", "16", "Wołowina, ser pleśniowy, karmelizowana cebula, pomidor, sałata, rukola, ketchup, bułka"]),
-            MealModel(value: ["Major", "18", "Wołowina, ser cheddar, bekon, papryka marynowana, papryka peperoni, sałata, karmelizowana cebula, pomidor, ketchup, sos cayenne (ostry), bułka"]),
-            MealModel(value: ["Kurczak w pomidorach", "16", "Filet z piersi kurczaka, ser mozarella, suszone pomidory, czerwona cebula, pesto, sałata, rukola, ketchup, majonez, bułka"]),
-            MealModel(value: ["Kurczak w pieczarkach", "16", "Filet z piersi kurczaka w curry, ser cheddar, pieczarki duszone, bekon, czerwona cebula, sałata, ketchup, majonez, bułka"]),
-            MealModel(value: ["Grek", "18", "Wołowina, tzatziki, ser feta, cebula czerwona, pomidor, sałata, ketchup, bułka"]),
-            MealModel(value: ["Pierogi", "10", "10 sztuk: ruskie, z mięsem, ze szpinakiem, z pieczarkami, z kapustą i grzybami, Gajowego - kasza, grzyby, boczek"])
-        ]
-        
-        let majorBurger: [Any?] = ["Major Catering", "692 830 954", majorMeals]
-        return FoodTruckModel(value: majorBurger)
-    }
-    
-    private static func rozbrykanaOwcaData() -> FoodTruckModel {
-        let owcaMeals: [MealModel] = [
-            MealModel(value: ["Owca w biegu", "10/18", "Kefta z mięsa jagnięcego, sos pomidorowo-kolendrowy z ciecierzycą, sałata z dwoma rodzajami sosów jogurtowych (pikantnym i sezamowo cytrynowym), grillowana pita"]),
-            MealModel(value: ["Owca na wypasie", "22", "Kefta z mięsa jagnięcego, falafele, sos pomidorowo-kolendrowy z ciecierzycą, sałata z dwoma rodzajami sosów jogurtowych (pikantnym i sezamowo cytrynowym), grillowana pita"]),
-            MealModel(value: ["Owca vege", "10/15", "Falafele, sos pomidorowo-kolendrowy z ciecierzycą, sałata z dwoma rodzajami sosów jogurtowych (pikantnym i sezamowo cytrynowym), grillowana pita"])
-        ]
-        
-        let rozbrykanaOwca: [Any?] = ["Rozbrykana Owca", "514 387 953", owcaMeals]
-        return FoodTruckModel(value: rozbrykanaOwca)
-    }
-    
-    
-    //FIXME: prepare more data
-    private static func mondayData() -> DayModel {
-        var foodTrucks: [FoodTruckModel] = [hindusData()]
-        let m = DayModel()
-        m.day = "Poniedziałek"
-        m.dayColor = DayColors.blue.rawValue
-        m.foodTrucks = List(foodTrucks)
-        let monday: [Any?] = ["Poniedziałek", foodTrucks, DayColors.blue.rawValue]
-//        return DayModel(value: monday)
-        return m
-    }
-    
-    private static func thursdayData() -> DayModel {
-        let foodTrucks: [FoodTruckModel] = [manufakturaData(), pizzaZPiecaData(), majorBurgerData(), rozbrykanaOwcaData()]
-        let t = DayModel()
-        t.day = "Czwartek"
-        t.dayColor = DayColors.orange.rawValue
-        t.foodTrucks = List(foodTrucks)
-        let thursday: [Any?] = ["Czwartek", foodTrucks, DayColors.orange.rawValue]
-//        return DayModel(value: thursday)
-        return t
+        for i in 0..<names.count {
+            let day = DayModel(value: [names[i], colors[i], foodTrucks[i]])
+            try! realm().write {
+                realm().add(day)
+            }
+        }
     }
 }
 
